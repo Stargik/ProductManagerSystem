ed8303b6ab0876623ee2f35888ea7be4b503fd0b
aleksandrstarzhynskyi <a.stargik@gmail.com>
2024-01-31 16:27:30 +0200

    Updated StatusCode entity

--- DAL/Entities/StockStatus.cs
+++ DAL/Entities/StockStatus.cs
@@ -4,6 +4,7 @@ namespace DAL.Entities
 	public class StockStatus : BaseEntity
 	{
 		public string Name { get; set; }
+		public int StatusCode { get; set; }
 
 		public List<Product> Products { get; set; }
 	}
